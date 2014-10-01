b4eee4ea3dbb46c62949b1c2f59db6cab63f109c
Jay McCarthy <jay.mccarthy@gmail.com>
2014-09-30 20:54:15 -0400

    More like OpenGL

--- mode-lambda/main.rkt
+++ mode-lambda/main.rkt
@@ -146,20 +146,8 @@
     ;; loop. I won't do this, because this is a "reference" software
     ;; renderer.
 
-    ;; xxx We are doing per-poly drawing, but we could use the
-    ;; "scanline algorithm": basically, sort the triangles by
-    ;; Y-position/X-pos, then do a single iteration over the screen (like
-    ;; the iteration of x/y in draw-triangle) and then go through the
-    ;; sorted lists of triangles and check out which triangle a point
-    ;; is part of and then use its fragment function to compute the
-    ;; color. This will be much more like how OpenGL works. Right now
-    ;; we are (2*S)*(W*H) because in the worst case each triangle
-    ;; could potentially cover the screen. The scanline algorithm
-    ;; would be (W*H)*(2*S) because in the worst case every triangle
-    ;; is on every line. I think in practice S_i is much smaller than
-    ;; W_k*H_k, so scanline should be faster
+    ;; XXX move bs* to globals / static-db
 
-    ;; XXX in OpenGL bs-* would be a global
     (define (fragment-shader fill! bs bs-w bs-h a r g b tx.0 ty.0)
       (define tx (inexact->exact (floor tx.0)))
       (define ty (inexact->exact (floor ty.0)))
@@ -175,6 +163,9 @@
       ;; This "unless" corresponds to discarding non-opaque pixels
       (unless (zero? na)
         (fill! na nr ng nb)))
+
+    ;; XXX separate into verticex-data & attributes
+    ;; XXX use cstructs for attributes
 
     ;; bx and ty are like "varying" vertex attributes that are
     ;; interpolated across the triangle. bs* are globals, and argb is
@@ -196,8 +187,12 @@
          (?flvector-ref v3 O)))
     (define (triangle-F-y F t) (triangle-F-O F 1 t))
     (define (triangle-min-y t) (triangle-F-y min t))
+    (define (triangle-min-x t) (triangle-F-O min 0 t))
     (define (triangle-max-y t) (triangle-F-y max t))
-    (define (point-in-triangle? t x y)
+    (define (triangle-min-x<= t1 t2)
+      (<= (triangle-min-x t1)
+          (triangle-min-x t2)))
+    (define (when-point-in-triangle t x y f)
       (match-define
        (triangle bs bs-w bs-h a r g b
                  v1 tx1 ty1
@@ -226,13 +221,10 @@
         (- 1 λ1 λ2))
       ;; This condition is when the point is actually in the
       ;; triangle.
-      (cond
-       [(and (<= 0 λ1 1)
-             (<= 0 λ2 1)
-             (<= 0 λ3 1))
-        (vector λ1 λ2 λ3)]
-       [else
-        #f]))
+      (when (and (<= 0 λ1 1)
+                 (<= 0 λ2 1)
+                 (<= 0 λ3 1))
+        (f λ1 λ2 λ3)))
 
     (define (geometry-shader output! s)
       (match-define (sprite-data dx dy r g b a spr pal mx my theta) s)
@@ -266,47 +258,49 @@
                  RU (+ start-tx spr-w) (+ start-ty spr-h)
                  RL (+ start-tx spr-w) start-tx)))
 
-    (define y->triangles (make-vector height null))
+    (local-require data/heap)
+    (define y->triangles (build-vector height (λ (i) (make-heap triangle-min-x<=))))
     (define (output! t)
       (for ([y (in-range (max 0
                               (inexact->exact (floor (triangle-min-y t))))
                          (min height
                               (inexact->exact (ceiling (triangle-max-y t)))))])
-        (vector-set! y->triangles y 
-                     (cons t (vector-ref y->triangles y)))))
+        (define yts (vector-ref y->triangles y))
+        (heap-add! yts t)))
     (tree-for (λ (s) (geometry-shader output! s)) sprite-tree)
 
     ;; Clear the screen
     (bytes-fill! root-bs 0)
-
-    (define (fill-screen!)
-      (for* ([y (in-range 0 height)]
-             [x (in-range 0 width)])
-        (let/ec drew
-          (for ([t (in-list (vector-ref y->triangles y))])
-            (match (point-in-triangle? t x y)
-              [(vector λ1 λ2 λ3)
-               (match-define
-                (triangle bs bs-w bs-h a r g b
-                          v1 tx1 ty1
-                          v2 tx2 ty2
-                          v3 tx3 ty3)
-                t)
-               (define tx (+ (* λ1 tx1) (* λ2 tx2) (* λ3 tx3)))
-               (define ty (+ (* λ1 ty1) (* λ2 ty2) (* λ3 ty3)))
-               (define (fill! na nr ng nb)
-                 (pixel-set! root-bs width height x y 0 na)
-                 (pixel-set! root-bs width height x y 1 nr)
-                 (pixel-set! root-bs width height x y 2 ng)
-                 (pixel-set! root-bs width height x y 3 nb)
-                 ;; This is like a "depth" test. If the fragment drew
-                 ;; anything, then skip the rest of the triangles
-                 (drew))
-               (fragment-shader fill! bs bs-w bs-h a r g b tx ty)]
-              [#f
-               (void)])))))
-
-    (fill-screen!)
+    ;; Fill the screen
+    (for ([y (in-range 0 height)])
+      (define yts-h (vector-ref y->triangles y))
+      (define ytsk (heap-count yts-h))
+      (unless (zero? ytsk)
+        (define yts (heap->vector yts-h))
+        (vector-set! y->triangles y #f)
+        (for ([x (in-range 0 width)])
+          (let/ec drew
+            (for ([t (in-vector yts)])
+              (when-point-in-triangle
+               t x y
+               (λ (λ1 λ2 λ3)
+                 (match-define
+                  (triangle bs bs-w bs-h a r g b
+                            v1 tx1 ty1
+                            v2 tx2 ty2
+                            v3 tx3 ty3)
+                  t)
+                 (define tx (+ (* λ1 tx1) (* λ2 tx2) (* λ3 tx3)))
+                 (define ty (+ (* λ1 ty1) (* λ2 ty2) (* λ3 ty3)))
+                 (define (fill! na nr ng nb)
+                   (pixel-set! root-bs width height x y 0 na)
+                   (pixel-set! root-bs width height x y 1 nr)
+                   (pixel-set! root-bs width height x y 2 ng)
+                   (pixel-set! root-bs width height x y 3 nb)
+                   ;; This is like a "depth" test. If the fragment drew
+                   ;; anything, then skip the rest of the triangles
+                   (drew))
+                 (fragment-shader fill! bs bs-w bs-h a r g b tx ty))))))))
 
     ;; XXX Add a white background to easily tell the difference
     ;; between border and background in Preview
@@ -324,7 +318,6 @@
       (define root-bm
         (make-bitmap width height))
       (send root-bm set-argb-pixels 0 0 width height root-bs)
-      #;(eprintf "WROTE ~v\n" root-bs)
       (send root-bm save-file "lambda.png" 'png 100 #:unscaled? #t))
     (void)))
 
