8c9f89032c52e2b6230be29b57567b4812823d58
Jay McCarthy <jay.mccarthy@gmail.com>
2014-11-04 14:07:52 -0500

    fix deps

--- info.rkt
+++ info.rkt
@@ -1,3 +1,4 @@
 #lang info
 (define collection 'multi)
-(define deps '("base"))
+(define deps '("base" "srfi-lite-lib" "draw-lib" "opengl" "htdp-lib" "pict-lib" "draw-lib"))
+(define build-deps '("rackunit-lib"))
