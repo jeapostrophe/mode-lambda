a6ab19a8638d1f74b4a55b6c6fd6cb870a010e36
Jay McCarthy <jay.mccarthy@gmail.com>
2015-03-21 17:22:33 -0500

    more copying

--- mode-lambda/backend/gl.rkt
+++ mode-lambda/backend/gl.rkt
@@ -14,25 +14,25 @@
          (define id (file->string id-path))))
 
 (define (print-shader-log glGetShaderInfoLog shader-name shader-id)
-    (define-values (infoLen infoLog)
-      (glGetShaderInfoLog shader-id 1024))
-    (unless (zero? infoLen)
-      (eprintf "Log of shader(~a):\n~a\n"
-               shader-name
-               (subbytes infoLog 0 infoLen))
-      (eprintf "Exiting...\n")
-      (exit 1)))
+  (define-values (infoLen infoLog)
+    (glGetShaderInfoLog shader-id 1024))
+  (unless (zero? infoLen)
+    (eprintf "Log of shader(~a):\n~a\n"
+             shader-name
+             (subbytes infoLog 0 infoLen))
+    (eprintf "Exiting...\n")
+    (exit 1)))
 
 (define-syntax-rule
-    (define&compile-shader VertexShaderId
-      GL_VERTEX_SHADER
-      ProgramId VertexShader)
-    (begin (define VertexShaderId (glCreateShader GL_VERTEX_SHADER))
-           (glShaderSource VertexShaderId 1 (vector VertexShader)
-                           (s32vector))
-           (glCompileShader VertexShaderId)
-           (print-shader-log glGetShaderInfoLog 'VertexShader VertexShaderId)
-           (glAttachShader ProgramId VertexShaderId)))
+  (define&compile-shader VertexShaderId
+    GL_VERTEX_SHADER
+    ProgramId VertexShader)
+  (begin (define VertexShaderId (glCreateShader GL_VERTEX_SHADER))
+         (glShaderSource VertexShaderId 1 (vector VertexShader)
+                         (s32vector))
+         (glCompileShader VertexShaderId)
+         (print-shader-log glGetShaderInfoLog 'VertexShader VertexShaderId)
+         (glAttachShader ProgramId VertexShaderId)))
 
 ;; Old Code from GB (crt.rkt)
 
@@ -787,17 +787,193 @@
 
   draw)
 
+;; Old copied stuff from GB (compile.rkt)
+
+(require racket/path
+         racket/class
+         racket/file
+         racket/pretty
+         racket/match
+         racket/draw
+         racket/function
+         racket/list
+         gb/lib/korf-bin
+         gb/lib/fstree
+         tools/apse/lib
+         tools/apse/db)
+
+(define (compile-static-stuff db atlas-p pal-p idx-p idx-bin-p)
+  ;; Make the atlas
+  (define atlas-indexes
+    (let ()
+      (define sprites (map (curry load-sprite db) (db-sprites db)))
+      (define images
+        (flatten
+         (for/list ([s (in-list sprites)])
+           (for/list ([img (in-vector (sprite-images s))]
+                      [i (in-naturals)])
+             (vector s i img)))))
+
+      (define-values
+        (tex-size places)
+        (pack (λ (s*i) (sprite-width (vector-ref s*i 0)))
+              (λ (s*i) (sprite-height (vector-ref s*i 0)))
+              images))
+      (define how-many-places (add1 (length places)))
+
+      (define atlas-bin
+        (make-bytes (* tex-size tex-size)))
+
+      (define index-values 4)
+      (define index-bytes-per-value 4)
+      (define index-bin
+        (make-bytes (* index-values index-bytes-per-value
+                       how-many-places)))
+
+      (define sprite->img-placements
+        (hash-set
+         (for/hasheq
+          ([s (in-list sprites)])
+          (values s
+                  (make-vector
+                   (vector-length (sprite-images s)) 0)))
+         'none #f))
+      (for ([pl (in-list places)]
+            [pi (in-naturals 1)])
+        (match-define (placement ax ay (vector s i img)) pl)
+        (define ps (hash-ref sprite->img-placements s))
+        (vector-set! ps i (vector pi ax ay)))
+
+      (begin0
+        (append
+         (list ";; sprite info")
+         (list `(define-sprite-atlas ,how-many-places ,tex-size))
+         (list ";; sprites")
+         (for/list ([(e vs) (in-hash sprite->img-placements)])
+           (match e
+             ['none
+              `(define-sprite none 0 0 (0))]
+             [(? sprite? s)
+              (define w (sprite-width s))
+              (define h (sprite-height s))
+              (define pis
+                (for/list ([v (in-vector vs)]
+                           [i (in-naturals)]
+                           [img (in-vector (sprite-images s))])
+                  (match-define (vector pi ax ay) v)
+
+                  (for* ([x (in-range w)]
+                         [y (in-range h)])
+                    (define palette-val
+                      (bytes-ref img
+                                 (byte-xy-offset w h x y)))
+
+                    (bytes-set! atlas-bin
+                                (+ (* tex-size (+ ay y)) (+ ax x))
+                                palette-val))
+
+                  (for ([v (in-list (list ax ay w h))]
+                        [o (in-naturals)])
+                    (real->floating-point-bytes
+                     v index-bytes-per-value
+                     (system-big-endian?) index-bin
+                     (+ (* index-values
+                           index-bytes-per-value
+                           pi)
+                        (* index-bytes-per-value o))))
+
+                  pi))
+
+              `(define-sprite ,(sprite-name s) ,w ,h ,pis)])))
+
+        (display-to-file (gzip-bytes atlas-bin)
+                         #:exists 'replace
+                         atlas-p)
+        (display-to-file (gzip-bytes index-bin)
+                         #:exists 'replace
+                         idx-bin-p))))  
+
+  ;; Make the palette
+  (define palette-indexes
+    (let ()
+      (define palettes (db-palettes db))
+      (define palette-depth 16)
+      (define pal-bm
+        (make-object bitmap% palette-depth (length palettes) #f #t))
+      (define pal-bm-dc (new bitmap-dc% [bitmap pal-bm]))
+
+      (begin0
+        (append
+         (list ";; palette info")
+         (list `(define-palette-atlas
+                  ,(length palettes)
+                  ,palette-depth))
+         (list ";; palettes")
+         (for/list ([pn (in-list palettes)]
+                    [y (in-naturals)])
+           (define p (load-palette db pn))
+           (for ([c (in-vector (palette-color%s p))]
+                 [x (in-naturals)])
+             (send pal-bm-dc set-pixel x y c))
+           `(define-palette ,(palette-name p) ,y)))
+
+        (send pal-bm save-file pal-p 'png 100))))
+
+  ;; Make index
+  (let ()
+    (with-output-to-file idx-p
+      #:exists 'replace
+      (λ ()
+        (printf "#lang racket/base\n")
+        (pretty-display
+         `(require gb/graphics/texture-atlas-lib))
+        (newline)
+        (for-each displayln atlas-indexes)
+        (newline)
+        (for-each pretty-display palette-indexes)))))
+
 ;; New Interface
 (require racket/contract
          mode-lambda/core
          mode-lambda/backend/lib)
 
 (define (stage-draw/dc csd width height)
+  (define texture-atlas-path (make-temporary-file "~a.bin.gz"))
+  (define texture-index-path (make-temporary-file "~a.idx.bin.gz"))
+  (define palette-atlas-path (make-temporary-file "~a.png"))
+  ;; xxx turn csd into t-a-p, t-i-p, and p-a-p
+  (define new-sprite-index
+    (compile-static-stuff csd texture-atlas-path palette-atlas-path texture-index-path))
+  ;; xxx
+  (define last-sprites #f)
+  (define draw-on-crt-b (box #f))
+  (define draw-sprites-b (box #f))
   (λ (layer-config sprite-tree)
+    ;; xxx turn sprite-tree into last-sprites
     (λ (w h dc)
-      (error 'stage-draw/dc "~e"
-             (vector csd width height layer-config sprite-tree
-                     w h dc)))))
+      (local-require racket/class)
+      (define glctx (send dc get-gl-context))
+      (unless glctx
+        (error 'draw "Could not initialize OpenGL!"))
+      (send glctx call-as-current
+            (λ ()
+              (unless (unbox draw-on-crt-b)
+                (set-box! draw-on-crt-b (make-draw-on-crt)))
+              (unless (unbox draw-sprites-b)
+                (set-box!
+                 draw-sprites-b
+                 (make-draw
+                  texture-atlas-path
+                  texture-index-path
+                  palette-atlas-path
+                  crt-width
+                  crt-height)))
+              (when last-sprites
+                ((unbox draw-on-crt-b)
+                 w h
+                 (λ () ((unbox draw-sprites-b)
+                        last-sprites))))
+              (send glctx swap-buffers))))))
 
 (define gui-mode 'gl-core)
 (provide
