1ee99cd9389621c1417421f55b28cbd343377568
Jay McCarthy <jay.mccarthy@gmail.com>
2015-04-07 18:36:59 -0400

    beauty

--- mode-lambda/backend/gl.rkt
+++ mode-lambda/backend/gl.rkt
@@ -75,223 +75,236 @@
                        pal-size pal-bs pal->idx)
    csd)
 
-  ;; Main rendering step
-  (define layer-program (glCreateProgram))
-  (bind-attribs/cstruct-info layer-program _sprite-data:info)
-
-  (define-shader-source layer-vert "gl/ngl.vertex.glsl")
-  (define-shader-source layer-fragment "gl/ngl.fragment.glsl")
-
-  (compile-shader GL_VERTEX_SHADER layer-program layer-vert)
-  (compile-shader GL_FRAGMENT_SHADER layer-program layer-fragment)
-
-  (define SpriteAtlasId (make-2dtexture))
-  (with-texture (GL_TEXTURE0 SpriteAtlasId)
-    (load-texture/bytes atlas-size atlas-size atlas-bs))
-  (define PaletteAtlasId (make-2dtexture))
-  (with-texture (GL_TEXTURE0 PaletteAtlasId)
-    (load-texture/bytes PALETTE-DEPTH pal-size pal-bs))
-  (define SpriteIndexId (make-2dtexture))
-  (with-texture (GL_TEXTURE0 SpriteIndexId)
-    (load-texture/float-bytes
-     INDEX-VALUES (vector-length idx->w*h*tx*ty)
-     (sprite-index->bytes idx->w*h*tx*ty)))
-
   (define LayerConfigId (make-2dtexture))
+  (define update-layer-config!
+    (let ()
+      (define last-layer-config #f)
+      
+      (λ (layer-config)
+        (unless (equal? layer-config last-layer-config)
+          (set! last-layer-config layer-config)
+          (with-texture (GL_TEXTURE3 LayerConfigId)
+            (load-texture/float-bytes LAYER-VALUES LAYERS
+                                      (layer-config->bytes layer-config)))))))
+
   (define LayerTargets
     (for/list ([i (in-range LAYERS)])
       (make-target-texture width height)))
-  
-  (define layer-fbo (make-fbo LayerTargets)) 
-  (for ([i (in-range LAYERS)])
-    (glBindFragDataLocation layer-program i (format "out_Color~a" i)))
-  
-  (glLinkProgram&check layer-program)
-  (with-program (layer-program)
-    (glUniform1i (glGetUniformLocation layer-program "SpriteAtlasTex")
-                 (gl-texture-index GL_TEXTURE0))
-    (glUniform1i (glGetUniformLocation layer-program "PaletteAtlasTex")
-                 (gl-texture-index GL_TEXTURE1))
-    (glUniform1i (glGetUniformLocation layer-program "SpriteIndexTex")
-                 (gl-texture-index GL_TEXTURE2))
-    (glUniform1i (glGetUniformLocation layer-program "LayerConfigTex")
-                 (gl-texture-index GL_TEXTURE3))
-    (glUniform1ui (glGetUniformLocation layer-program "ViewportWidth") width)
-    (glUniform1ui (glGetUniformLocation layer-program "ViewportHeight") height))
-  (define layer-vao (glGen glGenVertexArrays))
-  (define layer-vbo (glGen glGenBuffers))
-  (nest
-   ([with-vertexarray (layer-vao)]
-    [with-arraybuffer (layer-vbo)])
-   (define-attribs/cstruct-info _sprite-data:info))
+  (define render-layers!
+    (let ()
+      (define layer-program (glCreateProgram))
+      (bind-attribs/cstruct-info layer-program _sprite-data:info)
 
-  ;; Combining Step
-  (define combine-program (glCreateProgram))
-  (define-shader-source combine-vert "gl/combine.vertex.glsl")
-  (define-shader-source combine-fragment "gl/combine.fragment.glsl")
-  (compile-shader GL_VERTEX_SHADER combine-program combine-vert)
-  (compile-shader GL_FRAGMENT_SHADER combine-program combine-fragment)
-  (glLinkProgram&check combine-program)
-  (with-program (combine-program)
-    (glUniform1i (glGetUniformLocation combine-program "LayerConfigTex")
-                 (gl-texture-index GL_TEXTURE0))
-    (glUniform1iv (glGetUniformLocation combine-program "LayerTargets")
-                  LAYERS
-                  (list->s32vector
-                   (for/list ([i (in-range LAYERS)])
-                     (+ 1 i))))
-    (glUniform1ui (glGetUniformLocation combine-program "ViewportWidth") width)
-    (glUniform1ui (glGetUniformLocation combine-program "ViewportHeight") height))
-  (define combine-vao (glGen glGenVertexArrays))
+      (define-shader-source layer-vert "gl/ngl.vertex.glsl")
+      (define-shader-source layer-fragment "gl/ngl.fragment.glsl")
 
-  ;; Screen step
-  (define screen-tex (make-target-texture width height))
-  (define screen-fbo (make-fbo (list screen-tex)))
+      (compile-shader GL_VERTEX_SHADER layer-program layer-vert)
+      (compile-shader GL_FRAGMENT_SHADER layer-program layer-fragment)
 
-  (define screen-program (glCreateProgram))
+      (define SpriteAtlasId (make-2dtexture))
+      (with-texture (GL_TEXTURE0 SpriteAtlasId)
+        (load-texture/bytes atlas-size atlas-size atlas-bs))
+      (define PaletteAtlasId (make-2dtexture))
+      (with-texture (GL_TEXTURE0 PaletteAtlasId)
+        (load-texture/bytes PALETTE-DEPTH pal-size pal-bs))
+      (define SpriteIndexId (make-2dtexture))
+      (with-texture (GL_TEXTURE0 SpriteIndexId)
+        (load-texture/float-bytes
+         INDEX-VALUES (vector-length idx->w*h*tx*ty)
+         (sprite-index->bytes idx->w*h*tx*ty)))
 
-  (define-shader-source crt-fragment "gl/crt.fragment.glsl")
-  (define-shader-source crt-vert "gl/crt.vertex.glsl")
-  (define-shader-source std-fragment "gl/std.fragment.glsl")
-  (define-shader-source std-vert "gl/std.vertex.glsl")
+      (define layer-fbo (make-fbo LayerTargets))
+      (for ([i (in-range LAYERS)])
+        (glBindFragDataLocation layer-program i (format "out_Color~a" i)))
 
-  (define-values (screen-fragment screen-vert)
-    (match screen-mode
-      ['crt (values crt-fragment crt-vert)]
-      ['std (values std-fragment std-vert)]))
+      (glLinkProgram&check layer-program)
+      (with-program (layer-program)
+        (glUniform1i (glGetUniformLocation layer-program "SpriteAtlasTex")
+                     (gl-texture-index GL_TEXTURE0))
+        (glUniform1i (glGetUniformLocation layer-program "PaletteAtlasTex")
+                     (gl-texture-index GL_TEXTURE1))
+        (glUniform1i (glGetUniformLocation layer-program "SpriteIndexTex")
+                     (gl-texture-index GL_TEXTURE2))
+        (glUniform1i (glGetUniformLocation layer-program "LayerConfigTex")
+                     (gl-texture-index GL_TEXTURE3))
+        (glUniform1ui (glGetUniformLocation layer-program "ViewportWidth") width)
+        (glUniform1ui (glGetUniformLocation layer-program "ViewportHeight") height))
 
-  (compile-shader GL_FRAGMENT_SHADER screen-program screen-fragment)
-  (compile-shader GL_VERTEX_SHADER screen-program screen-vert)
+      (define layer-vao (glGen glGenVertexArrays))
+      (define layer-vbo (glGen glGenBuffers))
+      (nest
+       ([with-vertexarray (layer-vao)]
+        [with-arraybuffer (layer-vbo)])
+       (define-attribs/cstruct-info _sprite-data:info))
 
-  (glLinkProgram&check screen-program)
+      (define SpriteData-count 0)
+      (define *initialize-count* (* 2 512))
+      (define SpriteData #f)
+      (define DrawnMult 6)
 
-  (with-program (screen-program)
-    (glUniform1i (glGetUniformLocation screen-program "rubyTexture")
-                 (gl-texture-index GL_TEXTURE0))
-    (glUniform2fv (glGetUniformLocation screen-program "rubyInputSize")
-                  1 (f32vector (* 1. width) (* 1. height))))
-  
-  (define screen-vao (glGen glGenVertexArrays))
+      (define (install-object! i o)
+        (define-syntax-rule (point-install! Horiz Vert j ...)
+          (begin
+            (set-sprite-data-horiz! o Horiz)
+            (set-sprite-data-vert! o Vert)
+            (cvector-set! SpriteData (+ (* i DrawnMult) j) o)
+            ...))
+        (point-install! -1 +1 0)
+        (point-install! +1 +1 1 4)
+        (point-install! -1 -1 2 3)
+        (point-install! +1 -1 5))
 
-  ;; Drawing state
-  (define SpriteData-count 0)
-  (define *initialize-count* (* 2 512))
-  (define SpriteData #f)
-  (define DrawnMult 6)
+      (define (install-objects! t)
+        (tree-fold (λ (offset o)
+                     (install-object! offset o)
+                     (add1 offset))
+                   0 t))
 
-  (define (install-object! i o)
-    (define-syntax-rule (point-install! Horiz Vert j ...)
-      (begin
-        (set-sprite-data-horiz! o Horiz)
-        (set-sprite-data-vert! o Vert)
-        (cvector-set! SpriteData (+ (* i DrawnMult) j) o)
-        ...))
-    (point-install! -1 +1 0)
-    (point-install! +1 +1 1 4)
-    (point-install! -1 -1 2 3)
-    (point-install! +1 -1 5))
+      (λ (static-st dynamic-st)
+        ;; xxx optimize static
+        (define objects (cons static-st dynamic-st))
+        (define early-count (count-objects objects))
+        (define SpriteData-count:new (max *initialize-count* early-count))
 
-  (define (install-objects! t)
-    (tree-fold (λ (offset o)
-                 (install-object! offset o)
-                 (add1 offset))
-               0 t))
+        (nest
+         ([with-framebuffer (layer-fbo)]
+          [with-vertexarray (layer-vao)]
+          [with-vertex-attributes ((length _sprite-data:info))]
+          [with-texture (GL_TEXTURE0 SpriteAtlasId)]
+          [with-texture (GL_TEXTURE1 PaletteAtlasId)]
+          [with-texture (GL_TEXTURE2 SpriteIndexId)]
+          [with-texture (GL_TEXTURE3 LayerConfigId)]
+          [with-feature (GL_BLEND)]
+          [with-program (layer-program)])
+         (with-arraybuffer (layer-vbo)
+           (unless (>= SpriteData-count SpriteData-count:new)
+             (set! SpriteData-count
+                   (max (* 2 SpriteData-count)
+                        SpriteData-count:new))
+             (glBufferData GL_ARRAY_BUFFER
+                           (* SpriteData-count
+                              DrawnMult
+                              (ctype-sizeof _sprite-data))
+                           #f
+                           GL_STREAM_DRAW))
 
-  (define last-layer-config #f)
+           (set! SpriteData
+                 (make-cvector*
+                  (glMapBufferRange
+                   GL_ARRAY_BUFFER
+                   0
+                   (* SpriteData-count
+                      DrawnMult
+                      (ctype-sizeof _sprite-data))
+                   (bitwise-ior
+                    ;; We are overriding everything (this would be wrong if
+                    ;; we did the caching "optimization" I imagine)
+                    GL_MAP_INVALIDATE_RANGE_BIT
+                    GL_MAP_INVALIDATE_BUFFER_BIT
+
+                    ;; We are not doing complex queues, so don't block other
+                    ;; operations (but it doesn't seem to improve performance
+                    ;; by having this option)
+                    ;; GL_MAP_UNSYNCHRONIZED_BIT
+
+                    ;; We are writing
+                    GL_MAP_WRITE_BIT))
+                  _sprite-data
+                  (* SpriteData-count
+                     DrawnMult)))
+
+           (install-objects! objects)
+           (glUnmapBuffer GL_ARRAY_BUFFER))
+
+         (glDrawBuffers LAYERS
+                        (list->s32vector
+                         (for/list ([i (in-range LAYERS)])
+                           (GL_COLOR_ATTACHMENTi i))))
+         (glClearColor 0.0 0.0 0.0 0.0)
+         (glBlendFunc GL_SRC_ALPHA GL_ONE_MINUS_SRC_ALPHA)
+         (glClear GL_COLOR_BUFFER_BIT)
+         (glViewport 0 0 width height)
+         (glDrawArrays GL_TRIANGLES 0 (* DrawnMult early-count))))))
+
+  (define combine-tex (make-target-texture width height))
+  (define combine-layers!
+    (let ()
+      (define combine-program (glCreateProgram))
+      (define-shader-source combine-vert "gl/combine.vertex.glsl")
+      (define-shader-source combine-fragment "gl/combine.fragment.glsl")
+      (compile-shader GL_VERTEX_SHADER combine-program combine-vert)
+      (compile-shader GL_FRAGMENT_SHADER combine-program combine-fragment)
+      (glLinkProgram&check combine-program)
+      (with-program (combine-program)
+        (glUniform1i (glGetUniformLocation combine-program "LayerConfigTex")
+                     (gl-texture-index GL_TEXTURE0))
+        (glUniform1iv (glGetUniformLocation combine-program "LayerTargets")
+                      LAYERS
+                      (list->s32vector
+                       (for/list ([i (in-range LAYERS)])
+                         (+ 1 i))))
+        (glUniform1ui (glGetUniformLocation combine-program "ViewportWidth") width)
+        (glUniform1ui (glGetUniformLocation combine-program "ViewportHeight") height))
+      (define combine-vao (glGen glGenVertexArrays))
+      (define combine-fbo (make-fbo (list combine-tex)))
+
+      (λ ()
+        (nest
+         ([with-framebuffer (combine-fbo)]
+          [with-vertexarray (combine-vao)]
+          [with-texture (GL_TEXTURE0 LayerConfigId)]
+          [with-textures (1 LayerTargets)]
+          [with-program (combine-program)])
+         (glClearColor 0.0 0.0 0.0 0.0)
+         (glClear GL_COLOR_BUFFER_BIT)
+         (glViewport 0 0 width height)
+         (glDrawArrays GL_TRIANGLES 0 FULLSCREEN_VERTS)))))
+
+  (define draw-screen!
+    (let ()
+      (define screen-program (glCreateProgram))
+
+      (define-shader-source crt-fragment "gl/crt.fragment.glsl")
+      (define-shader-source crt-vert "gl/crt.vertex.glsl")
+      (define-shader-source std-fragment "gl/std.fragment.glsl")
+      (define-shader-source std-vert "gl/std.vertex.glsl")
+
+      (define-values (screen-fragment screen-vert)
+        (match screen-mode
+          ['crt (values crt-fragment crt-vert)]
+          ['std (values std-fragment std-vert)]))
+
+      (compile-shader GL_FRAGMENT_SHADER screen-program screen-fragment)
+      (compile-shader GL_VERTEX_SHADER screen-program screen-vert)
+
+      (glLinkProgram&check screen-program)
+
+      (with-program (screen-program)
+        (glUniform1i (glGetUniformLocation screen-program "rubyTexture")
+                     (gl-texture-index GL_TEXTURE0))
+        (glUniform2fv (glGetUniformLocation screen-program "rubyInputSize")
+                      1 (f32vector (* 1. width) (* 1. height))))
+
+      (define screen-vao (glGen glGenVertexArrays))
+
+      (λ (actual-screen-width actual-screen-height)
+        (nest
+         ([with-program (screen-program)]
+          [with-texture (GL_TEXTURE0 combine-tex)]
+          [with-vertexarray (screen-vao)])
+         (glUniform2fv (glGetUniformLocation screen-program "rubyOutputSize") 1
+                       (f32vector (* 1. actual-screen-width)
+                                  (* 1. actual-screen-height)))
+         (glClearColor 0.0 0.0 0.0 0.0)
+         (glClear GL_COLOR_BUFFER_BIT)
+         (glViewport 0 0 actual-screen-width actual-screen-height)
+         (glDrawArrays GL_TRIANGLES 0 FULLSCREEN_VERTS)))))
 
   (λ (actual-screen-width actual-screen-height layer-config static-st dynamic-st)
-    (unless (equal? layer-config last-layer-config)
-      (set! last-layer-config layer-config)
-      (with-texture (GL_TEXTURE3 LayerConfigId)
-        (load-texture/float-bytes LAYER-VALUES LAYERS
-                                  (layer-config->bytes layer-config))))
-
-    ;; xxx optimize static
-    (define objects (cons static-st dynamic-st))
-    (define early-count (count-objects objects))
-    (define SpriteData-count:new (max *initialize-count* early-count))
-
-    (nest
-     ([with-framebuffer (layer-fbo)]
-      [with-vertexarray (layer-vao)]
-      [with-vertex-attributes ((length _sprite-data:info))]
-      [with-texture (GL_TEXTURE0 SpriteAtlasId)]
-      [with-texture (GL_TEXTURE1 PaletteAtlasId)]
-      [with-texture (GL_TEXTURE2 SpriteIndexId)]
-      [with-texture (GL_TEXTURE3 LayerConfigId)]
-      [with-feature (GL_BLEND)]
-      [with-program (layer-program)])
-     (with-arraybuffer (layer-vbo)
-       (unless (>= SpriteData-count SpriteData-count:new)
-         (set! SpriteData-count
-               (max (* 2 SpriteData-count)
-                    SpriteData-count:new))
-         (glBufferData GL_ARRAY_BUFFER
-                       (* SpriteData-count
-                          DrawnMult
-                          (ctype-sizeof _sprite-data))
-                       #f
-                       GL_STREAM_DRAW))
-
-       (set! SpriteData
-             (make-cvector*
-              (glMapBufferRange
-               GL_ARRAY_BUFFER
-               0
-               (* SpriteData-count
-                  DrawnMult
-                  (ctype-sizeof _sprite-data))
-               (bitwise-ior
-                ;; We are overriding everything (this would be wrong if
-                ;; we did the caching "optimization" I imagine)
-                GL_MAP_INVALIDATE_RANGE_BIT
-                GL_MAP_INVALIDATE_BUFFER_BIT
-
-                ;; We are not doing complex queues, so don't block other
-                ;; operations (but it doesn't seem to improve performance
-                ;; by having this option)
-                ;; GL_MAP_UNSYNCHRONIZED_BIT
-
-                ;; We are writing
-                GL_MAP_WRITE_BIT))
-              _sprite-data
-              (* SpriteData-count
-                 DrawnMult)))
-
-       (install-objects! objects)
-       (glUnmapBuffer GL_ARRAY_BUFFER))
-
-     (glDrawBuffers LAYERS 
-                    (list->s32vector
-                     (for/list ([i (in-range LAYERS)]) 
-                       (GL_COLOR_ATTACHMENTi i))))
-     (glClearColor 0.0 0.0 0.0 0.0)
-     (glBlendFunc GL_SRC_ALPHA GL_ONE_MINUS_SRC_ALPHA)
-     (glClear GL_COLOR_BUFFER_BIT)
-     (glViewport 0 0 width height)
-     (glDrawArrays GL_TRIANGLES 0 (* DrawnMult early-count)))
-
-    (nest
-     ([with-framebuffer (screen-fbo)]
-      [with-vertexarray (combine-vao)]
-      [with-texture (GL_TEXTURE0 LayerConfigId)]
-      [with-textures (1 LayerTargets)]
-      [with-program (combine-program)])
-     (glClearColor 0.0 0.0 0.0 0.0)
-     (glClear GL_COLOR_BUFFER_BIT)
-     (glViewport 0 0 width height)
-     (glDrawArrays GL_TRIANGLES 0 FULLSCREEN_VERTS))
-
-    (nest
-     ([with-program (screen-program)]
-      [with-texture (GL_TEXTURE0 screen-tex)]
-      [with-vertexarray (screen-vao)])
-     (glUniform2fv (glGetUniformLocation screen-program "rubyOutputSize") 1
-                   (f32vector (* 1. actual-screen-width) (* 1. actual-screen-height)))
-     (glClearColor 0.0 0.0 0.0 0.0)
-     (glClear GL_COLOR_BUFFER_BIT)
-     (glViewport 0 0 actual-screen-width actual-screen-height)
-     (glDrawArrays GL_TRIANGLES 0 FULLSCREEN_VERTS))))
+    (update-layer-config! layer-config)
+    (render-layers! static-st dynamic-st)
+    (combine-layers!)
+    (draw-screen! actual-screen-width actual-screen-height)))
 
 (define (stage-draw/dc csd width height)
   (define draw
