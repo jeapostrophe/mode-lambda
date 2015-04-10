067342cd19af047a8e626e5e8e05c852c9a3041a
Jay McCarthy <jay.mccarthy@gmail.com>
2015-04-10 18:06:05 -0400

    removing texture refs inside fragment shader... might be bad?

--- mode-lambda/backend/gl/combine.fragment.glsl
+++ mode-lambda/backend/gl/combine.fragment.glsl
@@ -2,11 +2,12 @@
 @glsl-include["lib.glsl"]
 
 uniform sampler2D LayerTargets[@LAYERS];
-uniform sampler2D LayerConfigTex;
 uniform float Scale;
 uniform vec2 LogicalSize;
 
 in vec2 texCoord;
+flat in vec4 L_CY_HH_CX_HW[@LAYERS];
+flat in vec3 L_M7_H_F[@LAYERS];
 
 out vec4 oFragColor;
 
@@ -43,31 +44,36 @@ float compute_e(float ax, float hwidth, float fov, float pz, float Lcx, float Lh
 }
 
 void main() {
-  float width = LogicalSize.x;
-  float height = LogicalSize.y;
-  float hwidth = width / 2.0;
-  float hheight = height / 2.0;
+  float ax = texCoord.x;
+  float ay = texCoord.y;
   
-  vec4 fin_Color = vec4(0.0, 0.0, 0.0, 1.0);
-  float ax = texCoord.x * width;
-  float ay = height - texCoord.y * height;
+  float height = LogicalSize.y;
+  float hheight = height / 2.0;
+  float width = LogicalSize.x;
+  float hwidth = width / 2.0;
 
+  vec4 fin_Color = vec4(0.0, 0.0, 0.0, 1.0);
   for (int layer = 0 ; layer < @LAYERS ; layer++) {
-    @glsl-include["layer.glsl"]
+    float Lcy = L_CY_HH_CX_HW[layer].x;
+    float Lhh = L_CY_HH_CX_HW[layer].y;
+    float Lcx = L_CY_HH_CX_HW[layer].z;
+    float Lhw = L_CY_HH_CX_HW[layer].w;
+    float mode7coeff = L_M7_H_F[layer].x;
+    float horizon = L_M7_H_F[layer].y;
+    float fov = L_M7_H_F[layer].z;
+    
     float ay_horiz = horizon - ay;
     float pz = compute_pz(mode7coeff, ay_horiz);
-    if (! (pz <= 0.0)) {
-      float ey = compute_e(ay, hheight, fov, pz, Lcy, Lhh);
-      if (0.0 <= ey && ey <= height) {
-        float ex = compute_e(ax, hwidth, fov, pz, Lcx, Lhw);
-        if (0.0 <= ex && ex <= width) {
-          vec4 lay_Color = ctexture(LayerTargets[layer],
-                                    Scale * vec2(ex, (abs(ey - height))));
-          fin_Color.rgb =
-              fin_Color.rgb * (1.0 - lay_Color.a)
-            + lay_Color.rgb * lay_Color.a;
-        }
-      }
+    float ey = compute_e(ay, hheight, fov, pz, Lcy, Lhh);
+    float ex = compute_e(ax, hwidth, fov, pz, Lcx, Lhw);
+    vec4 lay_Color = ctexture(LayerTargets[layer],
+                              Scale * vec2(ex, (abs(ey - height))));
+    if ((! (pz <= 0.0))
+        && (0.0 <= ey && ey <= height)
+        && (0.0 <= ex && ex <= width)) {
+      fin_Color.rgb =
+          fin_Color.rgb * (1.0 - lay_Color.a)
+        + lay_Color.rgb * lay_Color.a;
     }
   }
   
--- mode-lambda/backend/gl/combine.vertex.glsl
+++ mode-lambda/backend/gl/combine.vertex.glsl
@@ -7,6 +7,8 @@ uniform sampler2D LayerConfigTex;
 uniform vec2 LogicalSize;
 
 out vec2 texCoord;
+flat out vec4 L_CY_HH_CX_HW[@LAYERS];
+flat out vec3 L_M7_H_F[@LAYERS];
 
 void main() {
   vec2 iTexCoord = compute_iTexCoord();
@@ -17,5 +19,18 @@ void main() {
               0.0, LogicalSize.y,
               1.0, -1.0);
 
-  texCoord = iTexCoord;	
+  float width = LogicalSize.x;
+  float height = LogicalSize.y;
+  float hwidth = width / 2.0;
+  float hheight = height / 2.0;
+  float ax = iTexCoord.x * width;
+  float ay = height - iTexCoord.y * height;
+
+  texCoord = vec2(ax,ay);
+
+  for (int layer = 0 ; layer < @LAYERS ; layer++) {
+    @glsl-include["layer.glsl"]
+    L_CY_HH_CX_HW[layer] = vec4(Lcy, Lhh, Lcx, Lhw);
+    L_M7_H_F[layer] = vec3(mode7coeff, horizon, fov);
+  }
 }
