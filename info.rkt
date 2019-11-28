a82f4a57892b4492a92acc7c73cd3b5e3d230144
Jack Firth <jackhfirth@gmail.com>
2019-11-28 14:40:57 -0800

    Add missing dependency
    The package server build is currently failing due to a missing dependency on `reprovide-lang-lib`.

--- info.rkt
+++ info.rkt
@@ -4,6 +4,7 @@
                "scheme-lib"
                "web-server-lib"
                "lux"
+               "reprovide-lang-lib"
                "base" "srfi-lite-lib" "draw-lib" "opengl" "htdp-lib" "pict-lib" "draw-lib"))
 (define build-deps '("draw-doc"
                      "racket-doc"
